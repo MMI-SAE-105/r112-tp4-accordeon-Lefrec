992b0f48c65ab3eb1fc35e9e4f85c5134e85b79b
Lefrec <blackfox244@gmail.com>
2024-11-26 17:21:13 +0100

    tests infructueux organisation verticale

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,2 @@
 const accordeon = document.querySelector(".accordeon");
-accordeon.addEventListener("click",(evt)=> accordeon.querySelectorAll("details").forEach((det) => (det.open = false)));
+accordeon.addEventListener("click",(evt)=> accordeon.querySelectorAll("details").forEach((elm) => elm.open = false));
